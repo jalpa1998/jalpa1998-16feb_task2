d3998e1966516441dcd8bcf5783e98ae4be1a3c7
jalpa1998 <162426649+jalpa1998@users.noreply.github.com>
2024-04-19 15:09:45 +0530

    Add files via upload

---
+++ task_19_march.cpp
@@ -0,0 +1,15 @@
+//user input tabel number
+#include <stdio.h>
+main()
+{
+    int num,i;
+	while(i=1) 
+    {
+        printf("\n\nEnter a number of tebal: ");
+        scanf("%d",&num);
+		for(i = 1; i <= 10; i++)
+        {
+            printf("\n%d * %d = %d", num, i, num * i);
+        }
+    }
+}
